4307a7993d39e69a7776f0f50482107ef7dca096
CODEmasterLEGEND <89576368+CODEmasterLEGEND@users.noreply.github.com>
2021-09-01 15:18:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,182 @@
+//Game States
+var PLAY=2;
+var END=3;
+var SERVE=1;
+var gameState=1;
+var score = 0;
+var knife,fruit ,monster,fruitGroup,monsterGroup, score,r,randomFruit, position;
+var knifeImage , fruit1, fruit2 ,fruit3,fruit4, monsterImage, gameOverImage;
+var home, invisiblehome2, invisiblehome21;
+var googpic, googpic2, googpic3, swap;
+function preload(){
+  
+  knifeImage = loadImage("knife.png");
+  monsterImage = loadAnimation("alien1.png","alien2.png")
+  fruit1 = loadImage("fruit1.png");
+  fruit2 = loadImage("fruit2.png");
+  fruit3 = loadImage("fruit3.png");
+  fruit4 = loadImage("fruit4.png");
+  gameOverImage = loadImage("gameover.png");
+  googpic = loadImage("home.jpg");
+  googpic2 = loadImage("over.jpg");
+  googpic3 = loadImage("cut.jpg");
+  //load sound here
+  swap = loadSound("knifeSwoosh.mp3");
+}
+
+
+
+function setup() {
+  createCanvas(800,500);
+  home=createSprite(400,250,30,30);
+  home.addImage(googpic);
+  home.scale = 3;
+
+
+  invisiblehome21 = createSprite(400,250,400,10);
+  invisiblehome21.addImage(googpic3);
+  invisiblehome21.scale = 3;
+  invisiblehome21.visible = false;
+
+  
+  
+  
+  invisiblehome2 = createSprite(400,250,400,10);
+  invisiblehome2.addImage(googpic2);
+  invisiblehome2.scale = 3;
+  invisiblehome2.visible = false;
+
+  //creating sword
+  knife=createSprite(40,200,20,20);
+  knife.addImage(knifeImage);
+  knife.scale=0.7
+ 
+ //set collider for sword
+ knife.setCollider("rectangle",0,0,40,40);
+
+ // Score variables and Groups
+ score=0;
+ fruitGroup=createGroup();
+ monsterGroup=createGroup();
+}
+
+function draw() {
+  background("lightblue");
+  if (gameState===SERVE) {
+    textSize(50);
+    fill("yellow");
+    text("press SPACE TO START",650,200);
+  if (keyDown("space")) {
+    gameState = PLAY;
+  }
+  
+  }
+  else if(gameState===PLAY){
+    home.destroy();
+    invisiblehome21.visible = true;
+    //Call fruits and Monster function
+    fruits();
+    Monster();
+   
+    // Move sword with mouse
+    knife.y=World.mouseY;
+    knife.x=World.mouseX;
+  
+    // Increase score if sword touching fruit
+    if(fruitGroup.isTouching(knife)){
+      fruitGroup.destroyEach();
+      score = score + 1;
+      swap.play();
+    }
+    else
+    {
+      // Go to end state if sword touching enemy
+      if(monsterGroup.isTouching(knife)){
+        gameState = END;
+        
+        //add gameover sound here
+        
+        fruitGroup.destroyEach();
+        monsterGroup.destroyEach();
+        fruitGroup.setVelocityXEach(0);
+        monsterGroup.setVelocityXEach(0);
+        
+        // Change the animation of sword to gameover and reset its position
+        
+      }
+    }
+  } 
+  else if (gameState===END) {
+    invisiblehome2.visible = true;
+    knife.destroy();
+  }
+  
+  drawSprites();
+  //Display score
+  textSize(25);
+  text("Score : "+ score,250,50);
+  if (gameState===SERVE) {
+    textSize(50);
+    fill("yellow");
+    text("press SPACE TO START",200,200);
+  }
+}
+
+
+function Monster(){
+  if(World.frameCount%200===0){
+    monster=createSprite(400,200,20,20);
+    monster.addAnimation("moving", monsterImage);
+    monster.y=Math.round(random(100,500));
+    //update below give line of code for increase monsterGroup speed by 10
+    monster.velocityX = -8;
+    monster.setLifetime=50;
+    
+    monsterGroup.add(monster);
+  }
+}
+
+function fruits(){
+  if(World.frameCount%80===0){
+    position = Math.round(random(1,2));
+    fruit=createSprite(400,200,20,20);
+    
+     //using random variable change the position of fruit, to make it more challenging
+    
+    if(position==1)
+    {
+    fruit.x=700;
+    //update below give line of code for increase fruitGroup speed by 4
+    fruit.velocityX=-7
+    }
+    else
+    {
+      if(position==2){
+      fruit.x=0;
+      
+     //update below give line of code for increase fruitGroup speed by 4
+      fruit.velocityX= 7;
+      }
+    }
+    
+    fruit.scale=0.2;
+     //fruit.debug=true;
+     r=Math.round(random(1,4));
+    if (r == 1) {
+      fruit.addImage(fruit1);
+    } else if (r == 2) {
+      fruit.addImage(fruit2);
+    } else if (r == 3) {
+      fruit.addImage(fruit3);
+    } else {
+      fruit.addImage(fruit4);
+    }
+    
+    fruit.y=Math.round(random(50,500));
+   
+    
+    fruit.setLifetime=100;
+    
+    fruitGroup.add(fruit);
+  }
+}
